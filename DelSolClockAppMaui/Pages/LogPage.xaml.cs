850597a0b02fcb0c9d47a3151ecd5e9a04860902
Marcus10110 <markgarrison@saleae.com>
2025-08-30 11:26:40 -0700

    removed old placeholder DelSolConnection class.

--- DelSolClockAppMaui/Pages/LogPage.xaml.cs
+++ DelSolClockAppMaui/Pages/LogPage.xaml.cs
@@ -5,14 +5,11 @@ namespace DelSolClockAppMaui.Pages;
 
 public partial class LogPage : ContentPage
 {
-    private DelSolConnection _ble = new();
 
     public LogPage()
     {
         InitializeComponent();
-        foreach( var log in _ble.GetLogs() )
-        {
-            LogStack.Children.Add( new Label { Text = log } );
-        }
+
+        LogStack.Children.Add( new Label { Text = "Example Log" } );
     }
 }
